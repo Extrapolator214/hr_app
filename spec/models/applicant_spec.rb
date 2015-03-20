8aa2c198e38a66e80ec3c1bfe12dcce6f6589b55
Grigory Reznichenko <gr.reznichenko@gmail.com>
2015-03-20 18:23:15 +0400

    setup test database, add applicant spec on name, install rspec-collection_matchers gem

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -15,8 +15,36 @@ require 'rails_helper'
 
 RSpec.describe Applicant, type: :model do
   context "Full Name" do
+    it "fails validation with no name" do
+      expect(Applicant.new).to have(1).error_on(:full_name)
+    end
+
+    it "fails validation with no name expecting a specific message" do
+      expect(Applicant.new.errors_on(:full_name)).to include("can't be blank")
+    end
+
+    it "fails validation with a 2 words name" do
+      expect(Applicant.new(:full_name => "Иванов Иван")).to have(1).errors_on(:full_name)
+    end
+
+    it "fails validation with a 4 words name" do
+      expect(Applicant.new(:full_name => "Иванов Иван Иванович Иванов")).to have(1).errors_on(:full_name)
+    end
+
+    it "fails validation with non Cyrillic letters" do
+      expect(Applicant.new(:full_name => "Ivanov Ivan Ivanovich")).to have(1).errors_on(:full_name)
+    end
+
+    it "fails validation with a special characters" do
+      expect(Applicant.new(:full_name => "Иванов Иван (Иванович)")).to have(1).errors_on(:full_name)
+    end
+
     it "passes validation if it contains 3 words consisting of Cyrillic letters and spaces" do
-      expect(Applicant.new(:name => "Иванов Иван Иванович")).to have(:no).errors_on(:name)
+      expect(Applicant.new(:full_name => "Иванов Иван Иванович")).to have(:no).errors_on(:full_name)
+    end
+
+    it "passes validation if it contains 3 words consisting of Cyrillic letters and multiple spaces" do
+      expect(Applicant.new(:full_name => " Иванов   Иван  Иванович ")).to have(:no).errors_on(:full_name)
     end
   end
 end
