66853c92b421efd419231b86fcbabd8e2cafd918
Grigory Reznichenko <gr.reznichenko@gmail.com>
2015-03-22 17:51:20 +0400

    applicant validation and spec

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -16,4 +16,24 @@ class Applicant < ActiveRecord::Base
   has_many :skills, through: :user_skills
 
   enum status: [:active, :idle]
+
+  before_save :strip_spaces_from_name
+
+  # presence is validated implicitly with the regex
+  validates :full_name, format: { with: /\A\s*(\p{Cyrillic}+\s+){2}\p{Cyrillic}+\s*\z/,
+                                          message: "3 слова, только кириллические символы и пробелы" }
+
+  validate :contact_info_email_or_phone
+
+  def strip_spaces_from_name
+    self.full_name = full_name.gsub(/\s{2,}/, ' ').strip if attribute_present?(:full_name)
+  end
+
+  def contact_info_email_or_phone
+    unless self.contact_info =~ /\A[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,8}\z/ || #email regex
+           self.contact_info =~ /\A\+[0-9]{11}\z/ # phone number in the format +71234567890
+      errors.add(:contact_info, "некорректный формат телефона или емейла")
+    end
+  end
+
 end
--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -19,10 +19,6 @@ RSpec.describe Applicant, type: :model do
       expect(Applicant.new).to have(1).error_on(:full_name)
     end
 
-    it "fails validation with no name expecting a specific message" do
-      expect(Applicant.new.errors_on(:full_name)).to include("can't be blank")
-    end
-
     it "fails validation with a 2 words name" do
       expect(Applicant.new(:full_name => "Иванов Иван")).to have(1).errors_on(:full_name)
     end
@@ -46,5 +42,40 @@ RSpec.describe Applicant, type: :model do
     it "passes validation if it contains 3 words consisting of Cyrillic letters and multiple spaces" do
       expect(Applicant.new(:full_name => " Иванов   Иван  Иванович ")).to have(:no).errors_on(:full_name)
     end
+
+    it "strips extra spaces from name" do
+      expect(Applicant.new(:full_name => " Иванов   Иван  Иванович ").strip_spaces_from_name).to eq "Иванов Иван Иванович"
+    end
   end
+
+  context "Contact Info" do
+    it "fails validation with no value" do
+      expect(Applicant.new).to have(1).error_on(:contact_info)
+    end
+
+    it "fails validation with a random value" do
+      expect(Applicant.new(:contact_info => "some_value 12# 1222-0")).to have(1).errors_on(:contact_info)
+    end
+
+    ['81234567890', '8-123-456-7890', '8-123-456-78-90', '+7 123 456 7890', '+7-123-456-78-90', '+7 (123) 456-78-90'].each do |phone|
+      it "fails validation if it contains phone number like this: #{phone}" do
+        expect(Applicant.new(:contact_info => phone)).to have(1).errors_on(:contact_info)
+      end
+    end
+
+    it "passes validation if it contains phone number in international format without spaces" do
+      expect(Applicant.new(:contact_info => "+71234567890")).to have(:no).errors_on(:contact_info)
+    end
+
+    ['test@test.com', 'test@fancy-domain.network', 'test.test_test.test@subdomain.subdomain.test.ru'].each do |email|
+      it "passes validation if it contains email like this: #{email}" do
+        expect(Applicant.new(:contact_info => email)).to have(:no).errors_on(:contact_info)
+      end
+    end
+
+    it "fails validation if it contains both phone number and email" do
+      expect(Applicant.new(:contact_info => "+7 123 456 7890, email@test.com")).to have(1).errors_on(:contact_info)
+    end
+  end
+
 end
