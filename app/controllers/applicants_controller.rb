5efb6ab036ed908d9b7e906b406784432564ba5d
Grigory Reznichenko <gr.reznichenko@gmail.com>
2015-03-22 21:17:00 +0400

    customize applicant views, add ru locale file

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -69,6 +69,6 @@ class ApplicantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def applicant_params
-      params[:applicant]
+      params.require(:applicant).permit!
     end
 end
